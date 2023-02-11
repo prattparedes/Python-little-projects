ede585a01e9cdb7604ddfe25469acb14f49055da
Pratt <pratt.paredes@gmail.com>
2023-02-10 22:41:05 -0500

    guess code function

--- game.py
+++ game.py
@@ -12,3 +12,23 @@ def generate_code():
         code.append(color)
 
     return code
+
+def guess_code():
+    while True:
+        guess = input("Guess: ").upper().split(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors.")
+            continue
+
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color}. Try again.")
+                break
+        else:
+            break
+
+    return guess
+
+def check_code(guess, real_code):
+    color_counts = {}
